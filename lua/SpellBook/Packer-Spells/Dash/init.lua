6bd696c25489166ac84522df0be62ecf4bfab694
Italo Amaya <italoamaya@Italos-MacBook-Pro.local>
2021-07-15 16:58:32 -0500

    made it pretty

--- lua/SpellBook/Packer-Spells/Dash/init.lua
+++ lua/SpellBook/Packer-Spells/Dash/init.lua
@@ -1,7 +1,7 @@
 -- vim.g.dashboard_default_executive = 'telescope'
 
 
-local headers = {{"coffee.cat", 42,80},{"dragon.cat",18,100},{"computer.cat",31,90},{"alien.cat", 45,80},{"earth.cat",43,100},{"maxpayne.cat",50,80},{"oldtv.cat",21,80},{"saturn.cat",36,80}}
+local headers = {{"coffee.cat", 42,80},{"dragon.cat",18,100},{"computer.cat",32,90},{"alien.cat", 45,80},{"earth.cat",43,100},{"maxpayne.cat",50,80},{"oldtv.cat",21,80},{"saturn.cat",36,80}}
 vim.g.dashboard_preview_command = 'cat'
 vim.g.dashboard_preview_pipeline = 'lolcat -F 0.03 -a -s 2000 -d 7'
 vim.g.dashboard_preview_file = '~/.config/nvim/lua/SpellBook/Packer-Spells/Dash/' .. headers[3][1]
@@ -21,5 +21,6 @@ vim.g.dashboard_custom_section = {
 
 --
 --
-vim.g.dashboard_custom_footer = {' '}
+vim.g.dashboard_custom_footer = {"Mostrosity :D "}
+
 --
